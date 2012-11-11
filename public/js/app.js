632bcbaa48c6fcd025c9d20ff37fc55aa030b446
Lamasu <lamasu@Kamino.(none)>
2012-11-10 23:16:32 -0200

    esqueleto do corpo do app

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+var app = {
+    models      : {},
+    controllers : {},
+    views       : {},
+    ajax        : {
+        get       : function (params) {
+            
+        },
+        put       : function (params) {
+            
+        },
+        post      : function (params) {
+            
+        },
+        del       : function (params) {
+            
+        }
+    },
+    ui          : {
+        flash     : function (message) {
+            
+        },
+        menu      : function () {
+            
+        },
+        header    : function () {
+            
+        },
+        container : function () {
+            
+        },
+        modal     : function () {
+            
+        },
+        confirm   : function () {
+            
+        },
+        start     : function () {
+            
+        }
+    }
+};
+
+(function () {
+    app.ui.start();
+})();
